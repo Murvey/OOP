b5bee8653020287d35b52e3732e7a0e4e17dcad7
Murvey <142988449+Murvey@users.noreply.github.com>
2023-11-23 18:26:12 +0100

    Update razlomak.cpp

--- Labos 6 - Prijatelji, statički članovi, iznimke, operatori (za ocjenu)/Zadatak 1 - Razlomak/razlomak.cpp
+++ Labos 6 - Prijatelji, statički članovi, iznimke, operatori (za ocjenu)/Zadatak 1 - Razlomak/razlomak.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+using namespace std;
 
+class Razlomak {
+    int brojnik, nazivnik;
+    static int counter;
+public:
+    Razlomak(int _brojnik, int _nazivnik) :brojnik(_brojnik), nazivnik(_nazivnik) {
+        if (brojnik / nazivnik == 0) {
+            counter++;
+        }
+    };
+
+    static int brojac() {
+        return counter;
+    }
+
+    void SetNazivnik(int br);
+
+};
+
+int Razlomak::counter = 0;
+
+void Razlomak :: SetNazivnik(int br) {
+    try {
+        if (br == 0) {
+            throw "RazlomakException";
+        }
+        nazivnik = br;
+    }
+    catch (const char* Iznimka) {
+        cout << "Iznimka:" << Iznimka;
+    }
+}
+
+int main() {
+    Razlomak A(2, 3);
+    A.SetNazivnik(0);
+
+    return 0;
+}
