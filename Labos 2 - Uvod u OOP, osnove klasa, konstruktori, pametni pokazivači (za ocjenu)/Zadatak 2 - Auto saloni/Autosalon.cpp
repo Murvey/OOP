0af97a9a6917090462f361221f57c0cb0352c3ef
Murvey <142988449+Murvey@users.noreply.github.com>
2023-11-23 17:59:30 +0100

    Update Autosalon.cpp

--- Labos 2 - Uvod u OOP, osnove klasa, konstruktori, pametni pokazivači (za ocjenu)/Zadatak 2 - Auto saloni/Autosalon.cpp
+++ Labos 2 - Uvod u OOP, osnove klasa, konstruktori, pametni pokazivači (za ocjenu)/Zadatak 2 - Auto saloni/Autosalon.cpp
@@ -1,3 +1,17 @@
+/*
+Klasa 'AutoSalon ' sadrži naziv, adresu, te popis automobila. Podaci o automobilu sadrže ime proizvođača automobila , vrstu goriva koje automobil koristi, snagu motora u KS i cijenu.
+a)    deklarirajte klase 'AutoSalon' i 'Automobil' te njihove članove tako da se pomoću njih može opisati navedena specifikacija
+b)    u funkciji main dodajte slijedeći programski odsječak te implementirajte sve potrebne metode da bi se mogao uspješno izvesti 
+vector<Automobil> auti = { Automobil("BMW 118i", "benzin", 150,2250), 
+        Automobil("Dacia Duster", "plin",  91, 114200),
+Automobil("Citroen C3", "diesel", 110, 127000)};
+
+        AutoSalon autosalon("Auto Salon", "Rijecka cesta 200, Rijeka", auti);// svi automobili u popisu automatski pripadaju tom autosalonu
+c)    u klasi AutoSalon deklarirajte metodu 'NajsnaznijiMotori' koja vraća popis svih automobila cija je snaga motora veća od 100 KS. Implementaciju metode napišite van klase
+d)    implementirajte metodu 'Sortiraj0' unutar klase AutoSalon koja će sortirati sve automobile počevši od onih koji imaju manju snagu, prema onima koji imaju veću
+e)    napišite globalnu funkciju 'NajskupljiAutoSalon' koja prima popis auto salona, a vraća onaj auto salon u kojemu su auti u prosjeku najskuplji 
+*/
+
 #include <iostream>
 #include <vector>
 
