5a44efce810690b1d430bce4e124b2649e100d66
Murvey <142988449+Murvey@users.noreply.github.com>
2023-12-11 22:36:02 +0100

    Update nadopuni.cpp

--- Labos 9 - Predlošci, funkcijski objekti, lambda funkcije (pripremna)/Zadatak 2 - Nadopuni/nadopuni.cpp
+++ Labos 9 - Predlošci, funkcijski objekti, lambda funkcije (pripremna)/Zadatak 2 - Nadopuni/nadopuni.cpp
@@ -20,3 +20,34 @@ cout << rez[i] << " ";
 
 
 */
+
+
+#include<iostream>
+#include<vector>
+#include<functional>
+using namespace std;
+
+vector<int> izdvoji(vector<int>& tmp, function<bool(int)>kriterij) { //& brzi prijenos jer nema kopiranja , moze i bez &
+	vector<int> pom;
+	for (int i = 0; i < tmp.size(); i++) {
+		if (kriterij(tmp[i])) {
+			pom.push_back(tmp[i]);
+		}
+	}
+	return pom;
+}
+bool djeljivS3(int a) {
+	return a % 3 == 0;
+}
+
+int main()
+{
+	vector<int> brojevi = { 1, 4, 5, 7, 3, 6, 12, 65, 32, 8, 87, 55, 23, 22, 1, 1, 433, 66, 7, 433, 3, 32, 76, 8, 72, 256, 42 };
+
+	vector<int> rez = izdvoji(brojevi, djeljivS3);//[](int n) {return (n % 3 == 0);} moze ovo umjesto definiranja globalne funkcije djejivS3
+
+	for (int i = 0; i < rez.size(); i++)
+		cout << rez[i] << " ";
+	//ispis: 3 6 12 87 66 3 72 42
+
+}
